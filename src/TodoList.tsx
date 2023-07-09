c66a8d7abe1b50edb715f4863c37dc1af5523313
Aliaksei <shebalinas2000@gmail.com>
2023-07-09 14:11:52 +0200

    TodoList isDone

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -76,7 +76,7 @@ export function TodoList(props: TodolistPropsType) {
     const titleMaxLengthWarning = isTitleLengthTooLong
         ? <div style={{color: 'darkred'}}>Title is too long</div>
         : null
-    const userMessage = errorsdsd
+    const userMessage = error
         ? <div style={{color: 'darkred'}}>Title is required!</div>
         : null
 
